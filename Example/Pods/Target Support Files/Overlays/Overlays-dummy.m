07de79b79096943ee074ce4e615a9f873e4adab5
Evgeniy Safronov <evsafronov.personal@yandex.ru>
2019-07-26 06:38:32 +0300

    Initial Commit

---
+++ Example/Pods/Target Support Files/Overlays/Overlays-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Overlays : NSObject
+@end
+@implementation PodsDummy_Overlays
+@end
---
+++ Example/Pods/Target Support Files/Pods-Overlays_Example/Pods-Overlays_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Overlays_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Overlays_Example
+@end
